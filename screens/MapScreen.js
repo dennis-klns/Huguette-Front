a7e49dd9d4e8dae94d574b9509a34e1c7fc0a0b3
Antho <rey.anthony1@gmail.com>
2024-03-04 10:41:20 +0100

    Add Map and Localisation on Mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,40 +1,57 @@
-import { StyleSheet, Dimensions, View, Modal, TextInput, TouchableOpacity, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import MapView from "react-native-maps";
 
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import * as Location from "expo-location";
+import { Marker } from "react-native-maps";
 
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-
-import * as Location from 'expo-location';
-import { Marker } from 'react-native-maps';
-
-
-export default function MapScreen() {
-
- 
+export default function MapScreen({ navigation }) {
+  const [currentPosition, setCurrentPosition] = useState(null);
 
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
 
-      if (status === 'granted') {
-        Location.watchPositionAsync({ distanceInterval: 10 },
-          (location) => {
-            setCurrentPosition(location.coords);
-          });
+      if (status === "granted") {
+        Location.watchPositionAsync({ distanceInterval: 10 }, (location) => {
+          setCurrentPosition(location.coords);
+        });
       }
     })();
   }, []);
 
-
- 
-
-
-
   return (
     <View style={{ flex: 1 }}>
-      {/* <Modal visible={modalVisible} transparent>
+      {currentPosition && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: currentPosition.latitude,
+            longitude: currentPosition.longitude,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: currentPosition.latitude,
+              longitude: currentPosition.longitude,
+            }}
+            title="Your Location"
+          />
+        </MapView>
+      )}
+      {/*<Modal visible={modalVisible} transparent>
 
         <View style={styles.centeredView}>
          <View style={styles.modalView}>
@@ -52,34 +69,28 @@ export default function MapScreen() {
        </View>
 
       </Modal> */}
-
-      <MapView style={{ flex: 1 }} onLongPress={(e) => handleLongPress(e)}>
-     
-      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   modalView: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 20,
     padding: 30,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -87,24 +98,21 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-},
+  },
 
-input: {
-  width: 150,
-  borderBottomColor: '#ec6e5b',
-  borderBottomWidth: 1,
-  fontSize: 16,
-},
+  input: {
+    width: 150,
+    borderBottomColor: "#ec6e5b",
+    borderBottomWidth: 1,
+    fontSize: 16,
+  },
 
-button: {
-  width: 150,
-  alignItems: 'center',
-  marginTop: 20,
-  paddingTop: 8,
-  backgroundColor: '#ec6e5b',
-  borderRadius: 10,
-},
-
-
+  button: {
+    width: 150,
+    alignItems: "center",
+    marginTop: 20,
+    paddingTop: 8,
+    backgroundColor: "#ec6e5b",
+    borderRadius: 10,
+  },
 });
-
