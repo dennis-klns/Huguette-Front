5a00505b0a0f1840405359d7ef9fb2f07106199b
Antho <rey.anthony1@gmail.com>
2024-03-07 14:42:26 +0100

    Recuperation des données de MapPositionScreen uniquement au dragEnd et transformation des données de localisation en adresse

--- screens/SignUpUserScreen.js
+++ screens/SignUpUserScreen.js
@@ -84,7 +84,7 @@ export default function SignUpUserScreen({ navigation }) {
 
     const formattedBirthdate = date.toISOString().split("T")[0];
 
-    fetch("http://192.168.10.156:3000/users/signup", {
+    fetch("https://huguette-backend.vercel.app/users/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -110,25 +110,6 @@ export default function SignUpUserScreen({ navigation }) {
         console.error("Error:", error);
       });
   };
-        fetch('https://huguette-backend.vercel.app/users/signup', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ lastname, firstname, email, phone, birthdate: formattedBirthdate, gender, password }),
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.result) {
-            dispatch(login({ phone, token: data.token })); //surement rajouter le email
-            navigation.navigate('SignUpPhoto');
-          } else {
-            console.error('Signup failed:', data.error);
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        })
-       
-      };
 
   const validateEmail = (value) => {
     const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
