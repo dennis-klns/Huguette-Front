a19ac228a002ca932c5004cf1eb54953a69ca20f
Your name <Your email>
2024-03-07 18:14:49 +0100

    profil picture start

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,3 +1,4 @@
+import { LinearGradient } from "expo-linear-gradient";
 import {
   Image,
   SafeAreaView,
@@ -7,105 +8,232 @@ import {
   View,
 } from "react-native";
 
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 export default function ProfileScreen({ navigation }) {
+
+  
   const handleSignIn = () => {
-    navigation.navigate("TabNavigator", { screen: "SignIn" });
+    navigation.navigate("TabNavigator", { screen: "Profile" });
   };
 
   const handleLogIn = () => {
-    navigation.navigate("TabNavigator", { screen: "Map" });
+    navigation.navigate("Home");
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={require("../assets/huguette.png")}
-        resizeMode="cover"
-        style={styles.image}
-      />
-      <View style={styles.textgroup}>
-        <Text style={styles.title}>Huguette</Text>
-        <Text style={styles.title}>Get Safe</Text>
-      </View>
-
-      <View style={styles.buttonsgroup}>
-        <TouchableOpacity
-          onPress={() => handleSignIn()}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.textButton}>S'inscrire</Text>
-        </TouchableOpacity>
+ 
+    <LinearGradient
+        colors={["#F1C796", "#EBB2B5", "#E0CAC2"]}
+        style={styles.linearGradient}
+    >
+        <SafeAreaView style={styles.container}>
 
         <TouchableOpacity
+  style={styles.bar}
+  
+  activeOpacity={0.3}
+>
+  <View style={styles.bar1}>
+    <View style={styles.bar11}>
+      <FontAwesome name="bolt" size={25} color="#ffffff" />
+    </View>
+    <View style={styles.bar12}>
+      <Text style={styles.textEmergency}>Conductrice/ Cliente</Text>
+    </View>
+  </View>
+  <View style={styles.bar2}>
+    <FontAwesome name="arrow-right" size={30} color="#ffffff" />
+  </View>
+</TouchableOpacity>
+
+          <TouchableOpacity style={styles.bar}>
+              <View style={styles.bar1}>
+                <View style={styles.bar11}>
+                   <FontAwesome name="user" size={25} color="#ffffff"  />
+                </View>
+                <View style={styles.bar12}>
+                      <TouchableOpacity style={styles.centerLine} activeOpacity={0.8}>
+                          <Text style={styles.textEmergency }>Informations personnelles</Text>
+                      </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.bar2}>
+                   <FontAwesome name="arrow-right" size={30} color="#ffffff"  />
+              </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bar}>
+              <View style={styles.bar1}>
+                <View style={styles.bar11}>
+                   <FontAwesome name="car" size={25} color="#ffffff"  />
+                </View>
+                <View style={styles.bar12}>
+                      <TouchableOpacity style={styles.centerLine} activeOpacity={0.8}>
+                          <Text style={styles.textEmergency }>Adresses préférées</Text>
+                      </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.bar2}>
+                   <FontAwesome name="arrow-right" size={30} color="#ffffff"  />
+              </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bar}>
+              <View style={styles.bar1}>
+                <View style={styles.bar11}>
+                   <FontAwesome name="money" size={25} color="#ffffff"  />
+                </View>
+                <View style={styles.bar12}>
+                      <TouchableOpacity style={styles.centerLine} activeOpacity={0.8}>
+                          <Text style={styles.textEmergency }>Paiement</Text>
+                      </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.bar2}>
+                   <FontAwesome name="arrow-right" size={30} color="#ffffff"  />
+              </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bar}>
+              <View style={styles.bar1}>
+                <View style={styles.bar11}>
+                   <FontAwesome name="phone" size={25} color="#ffffff"  />
+                </View>
+                <View style={styles.bar12}>
+                      <TouchableOpacity style={styles.centerLine} activeOpacity={0.8}>
+                          <Text style={styles.textEmergency }>Contacts d'urgence</Text>
+                      </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.bar2}>
+                   <FontAwesome name="arrow-right" size={30} color="#ffffff"  />
+              </View>
+          </TouchableOpacity>
+
+        
+
+          <View style={styles.buttonContainer}>
+          <TouchableOpacity
           onPress={() => handleLogIn()}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.textButton}>Se connecter</Text>
+          style={styles.button} activeOpacity={0.8} >
+          <Text style={styles.textButton}>Se déconnecter</Text>
         </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+        </View>
+
+           
+
+        </SafeAreaView>
+
+    </LinearGradient>
+
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+
+linearGradient: {
     flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-  },
+    paddingTop: '30%',
+},
 
-  image: {
-    height: "120%",
-    width: "100%",
-    position: "absolute",
-  },
-
-  title: {
-    width: "80%",
-    fontSize: 38,
-    fontWeight: "600",
-    textAlign: "center",
-    color: "white",
-  },
-
-  textgroup: {
+container: {
+  
     flex: 1,
-    justifyContent: "flex-end",
-  },
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+ 
+},
 
-  buttonsgroup: {
-    width: "80%",
-    alignItems: "center",
-    marginBottom: 30,
-    flex: 1,
-    justifyContent: "flex-end",
-  },
+bar:{
+  width: '100%',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingBottom:'6%',
+ 
+},
 
-  button: {
-    height: 40,
-    paddingTop: 8,
-    width: "80%",
-    alignItems: "center",
-    marginTop: 10,
-    backgroundColor: "white",
-    opacity: 0.5,
+bar1:{
+paddingLeft: '4%',
+width: '85%',
+flexDirection: 'row',
+alignItems: 'center',
+},
+
+bar2:{
+  paddingRight: '5%',
+},
+
+bar11:{
+width: '8%',
+justifyContent: 'center',
+alignItems: 'center',
+},
+
+bar12:{
+  paddingLeft: '4%',
+},
+
+startLine:{
+
+},
+
+centerLine: {
+    height: 45,
+    justifyContent: 'center',
+    alignItems: "flex-start",
+    backgroundColor: 'transparent',
     borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+
+},
+
+
+textEmergency: {
+    fontWeight: '800',
+    fontSize: 20,
+    marginRight: '10%',
+},
+
+endLine:{
+
+},
+
+buttonContainer:{
+  marginTop: '10%',
+  width: '60%',
+  alignItems: 'center',
+},
+
+button: {
+  
+  height: 40,
+  paddingTop: 8,
+  width: "60%",
+  alignItems: "center",
+  marginTop: 20,
+  backgroundColor: "#EAAC8B",
+  borderRadius: 10,
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2,
   },
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5,
+},
+
 
   textButton: {
-    color: "#000",
-    height: 30,
-    fontWeight: "600",
-    fontSize: 16,
+      color: '#fff',
+      height: 30,
+      fontWeight: '600',
+      fontSize: 16,
+      
   },
+
+
+
 });
