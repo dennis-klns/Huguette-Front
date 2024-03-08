c5609053bacf87a4ba42d17ba7ec835c8d462c68
Your name <Your email>
2024-03-08 15:48:40 +0100

    photoscreen updtate

--- screens/ProfilInformationsEditScreen.js
+++ screens/ProfilInformationsEditScreen.js
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  TextInput,
 } from "react-native";
 import Modal from 'react-native-modal'; 
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -39,19 +40,23 @@ export default function ProfilInformations({ navigation }) {
           </TouchableOpacity>
        </View>
 
-        <TouchableOpacity style={styles.bar} activeOpacity={0.3}>
-           <View style={styles.bar1}>
-               <View style={styles.bar11}>
-                  <FontAwesome name="bolt" size={25} color="#ffffff" />
-               </View>
-               <View style={styles.bar12}>
-                 <Text style={styles.textEmergency}>Conductrice/ Cliente</Text>
-               </View>
-           </View>
-           <View style={styles.bar2}>
-                <FontAwesome name="arrow-right" size={30} color="#ffffff" />
-           </View>
-        </TouchableOpacity>
+        <View style={styles.profilContainer} activeOpacity={0.3}>
+
+            <View style={styles.profile}>
+              <View>
+                <Text style={styles.text}>Modifier votre profil</Text>
+              </View>
+              <View>
+              <Text style={styles.text}>photo ici</Text>
+
+              </View>
+              <View>
+                  <TextInput placeholder="Lastname"/>
+                  <TextInput placeholder="Firstname"/>
+              </View>
+            </View>
+
+        </View>
 
 
        
@@ -77,125 +82,48 @@ container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
-  
- 
 },
 
 closeIcon: {
   width: '90%',
 },
 
-bar:{
+
+profilContainer:{
   width: '100%',
   flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'space-between',
   marginTop : '10%',
   paddingBottom:'6%',
+  borderWidth: 2,
  
 },
 
-bar1:{
-paddingLeft: '4%',
-width: '85%',
-flexDirection: 'row',
-alignItems: 'center',
-},
 
-bar2:{
-  paddingRight: '6%',
-},
-
-bar11:{
-width: '8%',
-justifyContent: 'center',
-alignItems: 'center',
-},
-
-bar12:{
-  paddingLeft: '4%',
-},
-
-startLine:{
-
-},
-
-centerLine: {
-    height: 45,
-    justifyContent: 'center',
-    alignItems: "flex-start",
-    backgroundColor: 'transparent',
-    borderRadius: 10,
-
-},
-
-
-textEmergency: {
-    fontWeight: '800',
-    fontSize: 20,
-    marginRight: '10%',
-},
-
-endLine:{
-
-},
-
-buttonContainer:{
-  marginTop: '13%',
-  width: '60%',
-  alignItems: 'center',
-},
-
-button: {
-  
-  height: 40,
-  paddingTop: 8,
-  width: "60%",
+profile: {
+  width: "100%",
+  justifyContent: 'center',
   alignItems: "center",
-  marginTop: 20,
-  backgroundColor: "#EAAC8B",
-  borderRadius: 10,
-  shadowColor: "#000",
-  shadowOffset: {
-    width: 0,
-    height: 2,
-  },
-  shadowOpacity: 0.25,
-  shadowRadius: 4,
-  elevation: 5,
+  borderWidth: 2,
+  
 },
 
+text: {
+  width: "80%",
+  fontSize: 16,
+  fontWeight: "800",
+  color: "#473E66",
+  margin: 10,
+},
 
-  textButton: {
-      color: '#fff',
-      height: 30,
-      fontWeight: '600',
-      fontSize: 16,
-      
-  },
-
-
-  texteModal:{
-    marginTop: 10,
-    fontSize: 15,
-  },
-  
-  modal: {
-    justifyContent: 'flex-end',
-    margin: 0,
-  },
-
-
-  modalContent: {
-    height: '70%',
-    backgroundColor: "white",
-    padding: 22,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 4,
-    borderColor: "rgba(0, 0, 0, 0.1)",
- 
-  },
-
+input: {
+  height: '10%',
+  width: "80%",
+  marginTop: 25,
+  borderBottomColor: "#4F4F4F",
+  borderBottomWidth: 1,
+  fontSize: 25,
+  color: "#4F4F4F",
+},
 
 });
