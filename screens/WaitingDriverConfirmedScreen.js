f097750c8dfc99bc9d790d1a03f511e716ddbff7
Dang-Minh TRAN <dang-minh.tran@hotmail.com>
2024-03-14 14:06:36 +0100

    WaitingDriverConfirmedScreen DONE

--- screens/WaitingDriverConfirmedScreen.js
+++ screens/WaitingDriverConfirmedScreen.js
@@ -13,9 +13,8 @@ import {
   View,
   Platform,
 } from "react-native";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
 import MapView from "react-native-maps";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useDispatch, useSelector } from "react-redux";
 
 import * as Location from "expo-location";
@@ -33,19 +32,11 @@ import {
 
 export default function MapScreen({ navigation }) {
   const [currentPosition, setCurrentPosition] = useState(null);
+  const [departure, setDeparture] = useState(null);
   //const [addresses, setAddresses] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [modalVisible2, setModalVisible2] = useState(false);
-  const [departure, setDeparture] = useState({});
-  const [arrival, setArrival] = useState({});
-  const [isAccompanied, setIsAccompanied] = useState(false);
-  const [mood, setMood] = useState(false);
-  const [music, setMusic] = useState(false);
   const [errorModalVisible, setErrorModalVisible] = useState(false);
-
-  const user = useSelector((state) => state.user.value);
-  const trip = useSelector((state) => state.trip.value);
-  const dispatch = useDispatch();
 
 
   const handleMapScreen = () => {
@@ -64,125 +55,6 @@ useEffect(() => {
 
 
 
-  // Essai en dur avec une liste d'adresses favorites - A SUPPRIMER UNE FOIS DYNAMIQUE
-  const addressesList = [
-    {
-      name: "Maison",
-      address: "16 rue des Boulets, PARIS",
-    },
-    {
-      name: "La Capsule",
-      address: "56 boulevard Pereire, PARIS",
-    },
-  ];
-
-
-
-  let iconStyleMusic = {};
-  let iconStyleMood = {};
-  if (music) {
-    iconStyleMusic = { color: "#EBB2B5" };
-  }
-
-  if (mood) {
-    iconStyleMood = { color: "#EBB2B5" };
-  }
-
-  const handleValidate = () => {
-    if (!arrival.completeAddress) {
-      setErrorModalVisible(true); // Affiche la modale d'erreur
-      return; // Empêche la navigation si les conditions ne sont pas remplies
-    }
-
-
-    fetch("https://huguette-backend.vercel.app/trips/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        longitudeD: departure.longitude,
-        latitudeD: departure.latitude,
-        longitudeA: arrival.longitude,
-        latitudeA: arrival.latitude,
-        tokenPassenger: user.token,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          console.log("OK:", trip);
-          dispatch(addTripId(data.trip._id));
-          dispatch(addDeparture(data.trip.departure.completeAddress));
-          dispatch(addArrival(data.trip.arrival.completeAddress));
-          dispatch(addDuration(data.trip.estimatedDuration));
-          dispatch(addDistance(data.trip.distance));
-          //dispatch(addCost(parseFloat(data.trip.estimatedDuration) * 30));
-          dispatch(addLongitude(data.trip.departure.longitude));
-          dispatch(addLatitude(data.trip.departure.latitude));
-          setArrival({});
-          setDeparture({});
-          setModalVisible(false);
-          navigation.navigate("MapPosition");
-
-          if (data.trip.estimatedDuration.includes("hour")) {
-            const str = data.trip.estimatedDuration;
-            const parts = str.split("mins").join("").split("hours");
-            const minutes = Math.floor(
-              Number(parts[0]) * 60 + Number(parts[1])
-            );
-            console.log(parts);
-            console.log(minutes);
-            dispatch(addCost(parseFloat(minutes) * 0.9));
-          } else {
-            dispatch(
-              addCost(Math.floor(parseFloat(data.trip.estimatedDuration) * 0.9))
-            );
-          }
-          if (data.trip.estimatedDuration.includes("hour")) {
-            const str = data.trip.estimatedDuration;
-            const parts = str.split("mins").join("").split("hours");
-            const minutes = Math.floor(
-              Number(parts[0]) * 60 + Number(parts[1])
-            );
-            console.log(parts);
-            console.log(minutes);
-            dispatch(addCost(Math.floor(parseFloat(minutes) * 0.9)));
-          } else {
-            dispatch(
-              addCost(Math.floor(parseFloat(data.trip.estimatedDuration) * 0.9))
-            );
-          }
-
-          console.log("tripBDD:", data.trip);
-        } else {
-          console.error("Failed:", data.error);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-
-      fetch("https://huguette-backend.vercel.app/users/moodPassenger", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        isAccompanied: isAccompanied,
-        token: user.token,
-        music: music,
-        mood: mood,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          console.log("Music changed:", data);
-        } else {
-          console.error("Failed Music:", data.error);
-        }
-      });
-  };
-
-  //console.log("tripReducer:", trip);
-
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
@@ -196,15 +68,6 @@ useEffect(() => {
     })();
   }, [modalVisible]);
 
-  // Affichage des adresses favorites
-  const addresses = addressesList.map((data, i) => {
-    return (
-      <View key={i} style={styles.addresses}>
-        <Text style={styles.name}>{data.name}</Text>
-        <Text>{data.address}</Text>
-      </View>
-    );
-  });
 
   return (
     <LinearGradient
@@ -233,7 +96,7 @@ useEffect(() => {
       <ScrollView >
         
       <View style={styles.search}>
-        <Text style={styles.title}>Votre chauffeuse Margueritte est en route !</Text>
+        <Text style={styles.title}>Votre conductrice Marguerite est en route !</Text>
         <Text style={styles.text}>Temps estimé : 4 minutes</Text>
         <View style={styles.buttonContainer}>
            <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.buttonScreen}>
@@ -246,7 +109,7 @@ useEffect(() => {
       <Modal visible={modalVisible} transparent={true} animationType="slide">
          <View style={styles.centeredView}>
             <View style={styles.modalContent}>
-               <Text style={styles.modalText}>Voulez-vous contacter votre chauffeur ?</Text>
+               <Text style={styles.modalText}>Voulez-vous contacter votre conductrice ?</Text>
                   <View style={styles.buttonModalContainer}> 
                      <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
                         <Text style={styles.textModal}>Message</Text>
@@ -261,7 +124,7 @@ useEffect(() => {
     <Modal visible={modalVisible2} transparent={true} animationType="slide">
          <View style={styles.centeredView}>
             <View style={styles.modalContent}>
-               <Text style={styles.modalText}>Voulez-vous annumler votre course ?</Text>
+               <Text style={styles.modalText}>Voulez-vous annuler votre course ?</Text>
                   <View style={styles.buttonModalContainer}> 
                      <TouchableOpacity style={styles.modalButton} onPress={() => handleMapScreen()} >
                         <Text style={styles.textModal}>Oui</Text>
@@ -290,144 +153,33 @@ const styles = StyleSheet.create({
     height: "70%",
   },
 
-  modalHeader: {
-    margin: 20,
-    height: Dimensions.get("window"),
-  },
-
-  addresse: {
-    width: "95%",
-    height: "35%",
-    borderBottomColor: "grey",
-    borderBottomWidth: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-
   search: {
-    paddingTop: '7%',
+    padding: '5%',
     width: "100%",
     justifyContent: "center",
   },
 
   title: {
-    fontSize: 40,
+    fontSize: 35,
     marginLeft: 10,
     fontFamily: "Ladislav-Bold",
   },
 
   text: {
-    fontSize: 20,
-    fontWeight: "400",
+    fontSize: 18,
+    fontWeight: "600",
     margin: 10,
     paddingTop: '2%',
-    fontFamily: "Ladislav-Bold",
   },
 
   // DEBUT DES ELEMENTS DE LA MODAL
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
 
-  shadowContainer: {
-    width: "80%",
-    height: "38%",
-    alignItems: "center",
-    borderRadius: 30,
-    backgroundColor: 'transparent', // Assurez-vous que le conteneur d'ombre est transparent
-    ...Platform.select({
-      android: {
-        elevation: 5,
-      },
-    }),
-  },
-  
-  profile: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
-    borderRadius: 30,
-    ...Platform.select({
-      ios: {
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-      },
-    }),
-  },
 
-  titlemodal: {
-    fontSize: 22,
-    marginLeft: 10,
-    fontFamily: "Ladislav-Bold",
-    textAlign: "center",
-    color: 'red',
-   
-  },
-
-  autoDeparture: {
-    height: "20%",
-    width: "90%",
-    zIndex: 150,
-  },
-
-  autoArrival: {
-    height: "20%",
-    width: "90%",
-    zIndex: 120,
-  },
-
-  isaccompanied: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    width: "100%",
-    marginTop: 30,
-  },
-
-  textmodal: {
-    fontSize: 16,
-    margin: 10,
+  textModal: {
+    fontSize: 15,
+    margin: 2,
     fontWeight: "600",
-  },
-
-  mood: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    width: "100%",
-    margin: 30,
-  },
-
-  icon: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "25%",
-  },
-
-  addresses: {
-    marginTop: 20,
-    color: "#000",
-    borderBottomColor: "#4F4F4F",
-    borderBottomWidth: 1,
-  },
-
-  scrollView: {
-    width: Dimensions.get("window").width,
-    padding: 30,
-  },
-
-  name: {
-    fontSize: 18,
-    fontWeight: "700",
+    color: 'white',
   },
 
   buttonContainer:{
@@ -438,25 +190,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingBottom: '10%',
     
-    
-  },
-
-  button: {
-    height: 40,
-    width: "60%",
-    alignItems: "center",
-    justifyContent: 'center',
-    marginTop: 20,
-    backgroundColor: "#F88559",
-    borderRadius: 30,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
   },
 
   buttonScreen: {
@@ -481,62 +214,7 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "600",
     fontSize: 16,
-    fontFamily: "Ladislav-Bold",
   },
-
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  errorModalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  
-
-  buttonClose: {
-    backgroundColor: "#F88559", // Couleur du bouton pour fermer la modale, ajustable
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-
-  // modalContent: {
-  //   backgroundColor: "white",
-  //   padding: 20,
-    
-  //   alignItems: "center",
-  //   borderRadius: 10,
-  //   borderColor: "rgba(0, 0, 0, 0.1)",
-  //   shadowColor: "#000",
-  //   shadowOffset: {
-  //     width: 0,
-  //     height: 2,
-  //   },
-  //   shadowOpacity: 0.25,
-  //   shadowRadius: 3.84,
-  //   elevation: 5,
-  // },
 
   modalContent: {
     backgroundColor: "white",
@@ -554,11 +232,6 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   
-  // modalText: {
-  //   fontSize: 20,
-  //   marginBottom: 15,
-  //   textAlign: "center",
-  // },
 
   modalText: {
     fontSize: 20,
@@ -571,20 +244,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    marginTop: 22,
   },
-  
-  modalButtonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-  },
-  
-  // buttonModalContainer:{
-  //   flexDirection: 'row',
-  //   width: '50%',
-  //   backgroundColor: 'blue',
-  // },
 
   buttonModalContainer: {
     flexDirection: 'row',
@@ -606,17 +267,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
     elevation: 3,
-  },
-  
-  // Ajoutez un style pour le texte des boutons dans la modale
-  modalButtonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "600",
-  },
-  
-  textModal: {
-    color: 'white',
   },
   
 
